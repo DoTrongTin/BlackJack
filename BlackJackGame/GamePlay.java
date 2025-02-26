767ea577324b4d5fbc38fcaa6425fa6ae61e39d2
DoTrongTin <tintrongdo162@gmail.com>
2025-02-26 08:09:38 +0700

    Enhance game logic to include additional win/tie conditions for Xi Bang and Ngu Linh

--- BlackJackGame/GamePlay.java
+++ BlackJackGame/GamePlay.java
@@ -15,11 +15,27 @@
         System.out.println("Dealer's hand: [" + dealer.getHand().getCards().get(0) + ", ?]");
         
 
-        if(player.getHand().isXiBang()){
+        if(player.getHand().isXiBang() && !dealer.getHand().isXiBang()){
             System.out.println("You win (Xi Bang)!");
             return;
         }
-        else if(player.getHand().isBlackJack() && !dealer.getHand().isBlac
+        else if(player.getHand().isXiBang() && dealer.getHand().isXiBang()){
+            System.out.println("Tie!");
+            return;
+        }
+        else if(player.getHand().isXiBang() && dealer.getHand().isBlackJack()){
+            System.out.println("You win (Xi Bang)!");
+            return;
+        }
+        else if(player.getHand().isBlackJack() && !dealer.getHand().isBlackJack()){
+            System.out.println("You win (BlackJack)!");
+            return;
+        }
+        else if(player.getHand().isBlackJack() && dealer.getHand().isBlackJack()){
+            System.out.println("Tie!");
+            return;
+        }
+
 
         while (!player.isBusted() && player.wantsToHit()) {
             player.drawCard(deck);
@@ -28,6 +44,11 @@
 
         if (player.isBusted()) {
             System.out.println("You lose (Busted)!");
+            return;
+        }
+
+        if(player.getHand().isNguLinh()){
+            System.out.println("You win (Ngu Linh)!");
             return;
         }
 
@@ -40,8 +61,11 @@
             System.out.println("You win!");
         } else if (player.getScore() == dealer.getScore()) {
             System.out.println("Tie!");
+        } else if(dealer.getHand().isNguLinh()){
+            System.out.println("You lose (Ngu Linh)!");
         } else {
             System.out.println("You lose!");
         }
     }
+    
 }
